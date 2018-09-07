151946096934a86dd25be54ecef0f9f00b06fc34
mnlcandelaria <mnl.candelaria@gmail.com>
2018-09-07 18:50:32 +0800

    feed activity show

--- scripts/custom.js
+++ scripts/custom.js
@@ -293,7 +293,7 @@ jQuery(function($) {
     formInquiry();
 
     setInterval(function() {
-        //activityFeed();
+        activityFeed();
     }, 10000);
 });
 
@@ -573,7 +573,7 @@ function activityFeed() {
     var temp;
 
     var request = $.ajax({
-        url: 'http://ico.openovate.com:4000/feed-activity',
+        url: 'https://api.monico.ph:8443/feed-activity',
         method: "GET",
         dataType: 'xml'
     });
